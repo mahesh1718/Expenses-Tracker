f59991a9ee9b953818f2e8ac682df45edff16b56
mahesh1718 <145094698+mahesh1718@users.noreply.github.com>
2025-01-11 07:52:18 +0530

    Update reactions.py

--- reactions.py
+++ reactions.py
@@ -1,16 +1,21 @@
 import emoji
 
+
 def sad():
-    return emoji.emojize(":disappointed:",language="alias")
+    return emoji.emojize(":disappointed:", language="alias")
+
 
 def happy():
-    return emoji.emojize(":grinning:",language="alias")
+    return emoji.emojize(":grinning:", language="alias")
+
 
 def fire():
-    return emoji.emojize(":fire:",language="alias")
+    return emoji.emojize(":fire:", language="alias")
+
 
 def congo():
-    return emoji.emojize(":tada:",language="alias")
+    return emoji.emojize(":tada:", language="alias")
+
 
 def winking():
-    return emoji.emojize(":wink:",language="alias")
+    return emoji.emojize(":wink:", language="alias")
